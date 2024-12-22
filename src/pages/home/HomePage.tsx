374ab59eda62ef78081deca3a83d3abd281606f4
Yisus Florez <juniniprogramador@outlook.com>
2024-12-22 16:39:57 -0500

    feature: added basic filter by name or description

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { loadImagesThunk } from "../../state/images";
 import { ImageCard } from "./components/ImageCard/ImageCard";
@@ -6,19 +6,44 @@ import { ImageForm } from "./components/ImageForm/ImageForm";
 
 export const HomePage = () => {
   const dispatch = useAppDispatch();
-  const imagesSelector = useAppSelector((state) => state.images);
+  const allImages = useAppSelector((state) => state.images);
+  const [filteredImages, setFilteredImages] = useState<any[]>([]);
+  console.log(allImages, filteredImages);
+  const filterImages = (searchTerm: string) => {
+    if (!searchTerm) {
+      return allImages.images;
+    }
+    return allImages.images.filter(
+      (image) =>
+        image.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        image.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
   useEffect(() => {
     dispatch(loadImagesThunk());
   }, []);
 
+  useEffect(() => {
+    if (allImages.images.length > 0) {
+      setFilteredImages(allImages.images);
+    }
+  }, [allImages]);
+
   return (
     <div>
       <input
+        onKeyUp={(e) => {
+          const searchTerm = e.currentTarget.value;
+          clearTimeout((e.currentTarget as any).searchTimeout);
+          (e.currentTarget as any).searchTimeout = setTimeout(() => {
+            setFilteredImages(filterImages(searchTerm));
+          }, 300);
+        }}
         type="text"
         className="bg-white border-2 rounded-full p-2 pl-4 mb-8"
       ></input>
       <div className="grid grid-cols-3 gap-6">
-        {imagesSelector.images.map((image) => (
+        {filteredImages.map((image) => (
           <ImageCard
             key={image.id}
             id={image.id}
@@ -29,7 +54,7 @@ export const HomePage = () => {
         ))}
       </div>
 
-      <ImageForm ></ImageForm>
+      <ImageForm></ImageForm>
     </div>
   );
 };
